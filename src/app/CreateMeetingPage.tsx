f3f4ba2c5a4ca051188c224503ff4c6a778580dc
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-09-10 22:28:19 +0700

    fix link meeting

--- src/app/CreateMeetingPage.tsx
+++ src/app/CreateMeetingPage.tsx
@@ -232,7 +232,7 @@ interface MeetingLinkProps {
 }
 
 function MeetingLink({ call }: MeetingLinkProps) {
-  const meetingLink = `/meeting/${call.id}`;
+  const meetingLink = `${process.env.NEXT_PRODUCTION_BASE_URL}/meeting/${call.id}`;
 
   return (
     <div className="flex flex-col items-center gap-3 text-center">
